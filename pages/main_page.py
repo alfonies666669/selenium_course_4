f9142b45861b05a9308610f795a868ec5d2f0d4a
alfonies666669 <50834183+alfonies666669@users.noreply.github.com>
2021-02-28 16:22:19 +0300

    step 4.3.8

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,13 +3,6 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .login_page import LoginPage
 
-class MainPage(BasePage): 
-
-    def go_to_login_page(self):
-        link = self.browser.find_element_by_css_selector("#login_link")
-        link.click()
-        alert = self.browser.switch_to.alert
-        alert.accept()
-		
-    def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
