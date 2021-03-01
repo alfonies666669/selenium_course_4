493c142ca6958db7ab5407b19536c47cbdc1143c
alfonies666669 <50834183+alfonies666669@users.noreply.github.com>
2021-03-01 18:32:25 +0300

    step 4.3.14(last)

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
 from .pages.product_page import ProductPage
-from .pages.login_page import LoginPage
 from .pages.BasketPage import BasketPage
+from .pages.login_page import LoginPage
 import pytest
 import random
 import time
@@ -11,15 +11,15 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
-    #product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message()
     product_page.should_be_book_price()
 
-def test_guest_cant_see_success_message(browser):
+def test_guest_cant_see_success_message(self, browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_not_be_success_message()
+
 
 @pytest.mark.xfail(reason="not disappiring")
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -28,19 +28,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_to_basket()
     product_page.is_dissappeared()
-	
+
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-	
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -50,8 +52,17 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page2.is_basket_empty()
     page2.should_be_basket_empty_message()
 
-class TestUserAddToBasketFromProductPage():
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_be_book_name()
+    product_page.should_be_book_price()
     
+
+class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
@@ -62,17 +73,20 @@ class TestUserAddToBasketFromProductPage():
         password = str(time.time() + count)
         self.login_page.register_new_user(email, password)
         self.login_page.should_be_authorized_user()
-		
+        
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         self.product_page = ProductPage(browser, link)
         self.product_page.open()
         self.product_page.should_not_be_success_message()
+
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         self.product_page = ProductPage(browser, link)
         self.product_page.open()
         self.product_page.add_to_basket()
+        #product_page.solve_quiz_and_get_code()
+        #time.sleep(120)
         self.product_page.should_be_book_name()
         self.product_page.should_be_book_price()
-    
